d52f1c3d9806b484fcfee4b21cb8c3dd8552e9c5
Dhruv Kaul <dhruvpkaul@gmail.com>
2025-07-14 19:59:11 -0500

    testing

--- PoC.py
+++ PoC.py
@@ -10,7 +10,6 @@ import easyocr
 import json
 from openai import OpenAI
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
@@ -46,15 +45,15 @@ def extract_frames(video_path, output_folder = "frames", fps = 1):
     cmd = f'ffmpeg -i "{video_path}" -vf "fps={fps}" {output_folder}/frame_%03d.png' # extracts still images from the video
     subprocess.run(cmd, shell=True)
     print(f"✅ Frames extracted to '{output_folder}' folder.")
- 
-# Step 3: Transcribe audi o(Using OpenAI Whisper)
+
+# Step 3: Transcribe audio (Using OpenAI Whisper)
 def transcribe_audio(video_path, model_size = "medium"):
     import whisper
     model = whisper.load_model(model_size) # loads the whisper model
     result = model.transcribe(video_path)
     print("✅ Transcription complete.")
 
-    return result["text"] # returns only the text 
+    return result["text"] # returns only the text
 
 # Step 4: Extract on screen text using OCR (Optical Character Recognition)
 def ocr_frames(frame_folder = "frames", num_frames = 5):
@@ -65,7 +64,7 @@ def ocr_frames(frame_folder = "frames", num_frames = 5):
     texts = []
 
     for filenmae in sorted(os.listdir(frame_folder))[:num_frames]: # loops through the first few image files (5 in our case)
-        if filenmae.endswith(".png"): # safety check 
+        if filenmae.endswith(".png"): # safety check
             frame_path = os.path.join(frame_folder, filenmae) # Builds the full path to each frame file
             results = reader.readtext(frame_path) # Runs OCR on the image using EasyOCR. Results is a list of results for each text box detected on the image.
 
@@ -76,7 +75,7 @@ def ocr_frames(frame_folder = "frames", num_frames = 5):
     print("✅ OCR complete.")
     return texts
 
-#Step 5 : Parse content extracted to standard9ze format
+# Step 5 : Parse content extracted to standard9ze format
 def create_recipe(caption, transcript, ocr_text):
     full_text = "caption: " + caption + " , transcript: "+ transcript + " , ocr text : " + " ".join(ocr_text)
 
@@ -114,7 +113,7 @@ def run_tiktok_parser(tiktok_url):
     print("\nPARSED RECIPE:\n", result_json)
     if os.path.exists("video/tiktok.mp4"):
         os.remove("video/tiktok.mp4")
-        os.rmdir("video")
+        # os.rmdir("video")
     if os.path.exists("frames"):
         for file in os.listdir("frames"):
             os.remove(os.path.join("frames", file))
